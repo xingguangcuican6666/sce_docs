8a7e4371015cc03a81519b3724e450bf485eb826
xingguangcuican6666 <xingguangcuican666@foxmail.com>
2025-09-21 20:02:11 +0800

    upload

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -91,9 +91,8 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Docs',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
